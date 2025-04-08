fe2e322a4f32920751a10bcb42499b95829694e9
Aditya Kandula <aditya.kandulaa@gmail.com>
2025-04-08 12:31:51 +0200

    Version 1

--- Lab2/Lab2_Group9.py
+++ Lab2/Lab2_Group9.py
@@ -1,11 +1,13 @@
+import copy
+
 class CSP:
     def __init__(self, variables, domains, constraints):
         self.variables = variables
         self.domains = domains
         self.constraints = constraints
         self.solution = None
-        self.viz = []  # For visualization steps
- 
+        self.viz = []
+
     def print_sudoku(self, puzzle):
         for i in range(9):
             if i % 3 == 0 and i != 0:
@@ -16,39 +18,39 @@ class CSP:
                     row_output += " |"
                 row_output += f" {puzzle[i][j]}"
             print(row_output.strip())
- 
+
     def visualize(self):
         print("\nStep-by-step visualization:")
         for step in self.viz:
             print(f"Assigned {step[2]} to ({step[0]}, {step[1]})")
- 
+
     def forward_checking(self, var, value, assignment):
-        row, col = var
-        temp_domains = {v: self.domains[v][:] for v in self.variables if v not in assignment}
- 
+        temp_domains = copy.deepcopy(self.domains)
         for peer in self.constraints[var]:
             if peer in temp_domains and value in temp_domains[peer]:
                 temp_domains[peer].remove(value)
-                if not temp_domains[peer]:
+                if not temp_domains[peer]:  # Domain wiped out
                     return None
         return temp_domains
- 
+
+
+
     def is_consistent(self, var, value, assignment):
         for neighbor in self.constraints[var]:
             if neighbor in assignment and assignment[neighbor] == value:
                 return False
         return True
- 
+
     def select_unassigned_variable(self, assignment):
+        # Can be optimized with MRV heuristic
         for var in self.variables:
             if var not in assignment:
                 return var
         return None
- 
+
     def backtrack(self, assignment):
         if len(assignment) == len(self.variables):
             return assignment
- 
         var = self.select_unassigned_variable(assignment)
         for value in self.domains[var]:
             if self.is_consistent(var, value, assignment):
@@ -64,15 +66,43 @@ class CSP:
                     self.domains = old_domains
                 del assignment[var]
         return None
- 
+
+    def is_valid_puzzle(self, puzzle):
+        def has_duplicates(block):
+            nums = [num for num in block if num != 0]
+            return len(nums) != len(set(nums))
+
+        # Check rows
+        for row in puzzle:
+            if has_duplicates(row):
+                return False
+
+        # Check columns
+        for col in zip(*puzzle):
+            if has_duplicates(col):
+                return False
+
+        # Check 3x3 boxes
+        for box_x in range(0, 9, 3):
+            for box_y in range(0, 9, 3):
+                box = [puzzle[i][j] for i in range(box_x, box_x+3) for j in range(box_y, box_y+3)]
+                if has_duplicates(box):
+                    return False
+
+        return True
+
     def solve(self):
+        if not self.is_valid_puzzle(puzzle):
+            print("Incorrect puzzle")
+            return None
         assignment = {}
         self.solution = self.backtrack(assignment)
         return self.solution
- 
-# ---------------- SETUP --------------------
+
+
+# ---------- SETUP -------------
 puzzle = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
-          [0, 0, 0, 1, 0, 5, 0, 0, 0],
+          [0, 7, 0, 1, 0, 5, 0, 0, 0],
           [0, 9, 8, 0, 0, 0, 0, 6, 0],
           [0, 0, 0, 0, 0, 3, 0, 0, 1],
           [0, 0, 0, 0, 0, 0, 0, 0, 6],
@@ -80,36 +110,42 @@ puzzle = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
           [0, 0, 0, 0, 0, 0, 0, 0, 8],
           [0, 0, 0, 0, 0, 0, 0, 1, 0],
           [0, 0, 0, 0, 0, 0, 4, 0, 0]]
- 
+
 variables = [(i, j) for i in range(9) for j in range(9) if puzzle[i][j] == 0]
-domains = {(i, j): list(range(1, 10)) for i, j in variables}
+
+# Constraints map
 constraints = {}
- 
-for i in range(9):
-    for j in range(9):
-        if puzzle[i][j] == 0:
-            peers = set()
-            # row and column
-            peers.update(((i, y) for y in range(9) if y != j))
-            peers.update(((x, j) for x in range(9) if x != i))
-            # 3x3 box
-            box_x, box_y = 3 * (i // 3), 3 * (j // 3)
-            for x in range(box_x, box_x + 3):
-                for y in range(box_y, box_y + 3):
-                    if (x, y) != (i, j):
-                        peers.add((x, y))
-            constraints[(i, j)] = peers
- 
-print('*' * 7, 'Solution', '*' * 7)
+for i, j in variables:
+    peers = set()
+    peers.update(((i, y) for y in range(9) if y != j))  # row
+    peers.update(((x, j) for x in range(9) if x != i))  # column
+    box_x, box_y = 3 * (i // 3), 3 * (j // 3)
+    for x in range(box_x, box_x + 3):
+        for y in range(box_y, box_y + 3):
+            if (x, y) != (i, j):
+                peers.add((x, y))
+    constraints[(i, j)] = peers
+
+# Initial domain pruning
+domains = {}
+for i, j in variables:
+    used = set()
+    used.update(puzzle[i])  # row
+    used.update(puzzle[x][j] for x in range(9))  # column
+    box_x, box_y = 3 * (i // 3), 3 * (j // 3)
+    used.update(puzzle[x][y] for x in range(box_x, box_x+3) for y in range(box_y, box_y+3))
+    domains[(i, j)] = [d for d in range(1, 10) if d not in used]
+
 csp = CSP(variables, domains, constraints)
 sol = csp.solve()
- 
+
 solution = [row[:] for row in puzzle]
 if sol:
     for (i, j), val in sol.items():
         solution[i][j] = val
- 
-    csp.print_sudoku(solution)
     csp.visualize()
+    print('*' * 7, 'Solution', '*' * 7)
+    csp.print_sudoku(solution)
+    
 else:
     print("Solution does not exist")
